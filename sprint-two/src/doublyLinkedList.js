749d55f350b55856abb8e7880a3df12336a8c46f
Luke Eastman <LukeAEastman@gmail.com>
2020-08-20 17:34:31 -0700

    implement doubly linked list and partial test suite

---
+++ sprint-two/src/doublyLinkedList.js
@@ -0,0 +1,64 @@
+var DoublyLinkedList = function() {
+  var list = {};
+  list.head = null;
+  list.tail = null;
+
+  list.addToTail = function(value) {
+    let node = new DNode(value);
+    if (list.tail === null) {
+      list.tail = node;
+      list.head = node;
+    } else {
+      node.previous = list.tail;
+      list.tail.next = node;
+      list.tail = node;
+    }
+  };
+
+  list.removeHead = function() {
+    if (list.head === null) {
+      return null;
+    }
+    let head = list.head;
+    if (list.head.next) {
+      list.head = list.head.next;
+      list.head.previous = null;
+    }
+    return head.value;
+  };
+
+  list.removeTail = function() {
+    if (list.tail === null) {
+      return null;
+    }
+    let tail = list.tail;
+    list.tail = list.tail.previous;
+    list.tail.next = null;
+    return tail.value;
+  };
+
+  list.contains = function(target) {
+    let nsearch = (node) => {
+      if (node === null) {
+        return false;
+      } else if (node.value === target) {
+        return true;
+      } else {
+        return nsearch(node.next);
+      }
+    };
+    return nsearch(list.head);
+  };
+
+  return list;
+};
+
+var DNode = function(value) {
+  var node = {};
+
+  node.value = value;
+  node.next = null;
+  node.previous = null;
+
+  return node;
+};
